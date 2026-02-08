98cbac93f10a20e04a2858ac3a8307f8fe335d54
Galbfran <francogalbiati984@gmail.com>
2026-02-08 10:22:53 -0300

    se agrega oneccion con db y entidad user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { UserEntity } from './user/entities/user.entity';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'prueba_bun',
+      entities: [
+        UserEntity,
+      ],
+      synchronize: true,
+    }),
+    UserModule,
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
