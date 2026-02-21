983a5536a25db3a6aac444f6fed6c5079b052446
Galbfran <francogalbiati984@gmail.com>
2026-02-21 10:40:51 -0300

    se agrega .env , .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,16 @@ import { UserEntity } from './user/entities/user.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'prueba_bun',
+      type: (process.env.DB_TYPE as any) || 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_DATABASE || 'prueba_bun',
       entities: [
         UserEntity,
       ],
-      
-      synchronize: true,
+      synchronize: process.env.DB_SYNCHRONIZE === 'true' || false,
     }),
     UserModule,
 
